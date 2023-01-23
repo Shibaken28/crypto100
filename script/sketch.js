6a2e8d24ebf2f65b3b976436f3fba87738e01414
Shibaken28 <8128kanzensu@gmail.com>
2023-01-23 11:58:47 +0900

    feat: add problems (but not asetting answer)

--- script/sketch.js
+++ script/sketch.js
@@ -1,15 +1,18 @@
 let problems = {};
 let jsondata;
 let cursorPos = new Object();
+let selecting = "";
 let selected = "";
 let trying = false;
+let debug = true;
 
 let dialogSwitch = document.getElementById("dialog-1");
 let dialog = document.getElementById("dialog");
+let dialogText = document.getElementById("dialog-text");
+let answer = document.getElementById("input");
 
 dialogSwitch.checked  = false;
 dialog.hidden = true;
-
 
 // 起動時の処理
 window.addEventListener("load", ()=>{
@@ -46,6 +49,8 @@ app.stage.addChild(container);
 
 container.pivot.x = 0;
 container.pivot.y = 0;
+container.scale.x = 1.0;
+container.scale.y = 1.0;
 container.x = 0;
 container.y = 0;
 container.interactive = true;
@@ -125,6 +130,8 @@ function initProblems(){
         problems[p.id].status = "lock";
         problems[p.id].require = p.require;
         problems[p.id].letter = p.letter;
+        problems[p.id].hash = p.answer;
+        problems[p.id].path = p.path;
         problems[p.id].block = new PIXI.Graphics();
         blocks.addChild(problems[p.id].block);
     }
@@ -156,19 +163,49 @@ function drawProblems(){
         let status = problems[id].status;
         block = problems[id].block;
         block.clear();
+        /*
         if(status=="win"){
             c = 0x00E853;
         }else if(status=="trying"){
             c = 0xF01222;
         }
         
-        block.lineStyle(10, 0x111111, 0.3);//第3引数透明度
-        block.beginFill(0x111111, 0.3);
-        block.drawRoundedRect(5, 5, blockSize, blockSize, 16);
+        block.lineStyle(0, 0, 0);//第3引数透明度
+        block.beginFill(0x111111, 0.5);
+        block.drawRoundedRect(15, 15, blockSize, blockSize, 16);
         block.endFill();
 
-        block.lineStyle(10, c, 0.5);//第3引数透明度
+        block.lineStyle(7, c, 1);//第3引数透明度
         block.beginFill(0xFAFAFA, 1);
+        block.drawRoundedRect(0, 0, blockSize, blockSize, 16);
+        block.endFill();
+        */
+        if(status=="win"){
+            c = 0x00E863;
+        }else if(status=="trying"){
+            c = 0xEEEEEE;
+        }else if(status=="lock"){
+            c = 0x111111;
+        }
+        block.lineStyle(0, 0, 0);//第3引数透明度
+        block.beginFill(0x030303, 0.5);
+        block.drawRoundedRect(15, 15, blockSize, blockSize, 16);
+        block.endFill();
+        let c2 = 0x999999;
+        let lv = problems[id].level;
+        if(lv==1){
+            c2 = 0x00F850;
+        }else if(lv==2){
+            c2 = 0x00E8E8;
+        }else if(lv==3){
+            c2 = 0x0020E8;
+        }else if(lv==4){
+            c2 = 0xEEEE00;
+        }else if(lv==5){
+            c2 = 0xFF1111;
+        }
+        block.lineStyle(5, c2, 0.9);//第3引数透明度
+        block.beginFill(c, 1);
         block.drawRoundedRect(0, 0, blockSize, blockSize, 16);
         block.endFill();
 
@@ -202,7 +239,7 @@ function drawProblems(){
 
 function checkMouseOver() {
     let mp = container.toLocal(cursorPos);
-    selected = "";
+    selecting = "";
     for (let id of Object.keys(problems)) {
         let blockSize = 100;
         let half = blockSize/2;
@@ -210,7 +247,7 @@ function checkMouseOver() {
         if(pos[0] - half <= mp.x && mp.x <= pos[0] + half && pos[1] - half <= mp.y && mp.y <= pos[1] + half){
             problems[id].block.scale.x = 1.2;
             problems[id].block.scale.y = 1.2;
-            selected = id;
+            selecting = id;
         }else{
             problems[id].block.scale.x = 1.0;
             problems[id].block.scale.y = 1.0;
@@ -219,17 +256,41 @@ function checkMouseOver() {
 }
 
 function onClick() {
-    if(selected==""){
+    if(selecting==""){
         dialogSwitch.checked = false;
         trying = false;
         return;
     }
-    if(problems[selected].status=="lock")return;
+    if(trying)return;
+    if(!debug&&problems[selecting].status=="lock")return;
+    selected = selecting;
     //問題dialogを表示
     dialogSwitch.checked = true;
     dialog.hidden = false;
     trying = true;
-    dialog.source = "./problems/html/"+selected+".html\"";
+    dialogText.src = "./problems/html/"+problems[selected].path;
 }
 
+function checkAnswer(){
+    hashed = sha256("thisisstringassalt.uwu"+answer.value);
+    hashed.then(value => {
+        if(value == problems[selected].hash){
+            problems[selected].status = "win";
+            checkUnlock();
+            drawProblems();
+            dialogSwitch.checked = false;
+            trying = false;
+        }else{
+            
+        }
+    }
+    );
+}
 
+async function sha256(str) {
+      // Convert string to ArrayBuffer
+      const buff = new Uint8Array([].map.call(str, (c) => c.charCodeAt(0))).buffer;
+      // Calculate digest
+      const digest = await crypto.subtle.digest('SHA-256', buff);
+      return [].map.call(new Uint8Array(digest), x => ('00' + x.toString(16)).slice(-2)).join('');
+  }
